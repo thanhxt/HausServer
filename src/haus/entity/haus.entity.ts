2afb8c684aa60b5d3b05c58eb6da9940c4a4b4b6
thanhxt <thanhtran_xuan@hotmail.de>
2024-11-05 19:13:46 +0100

    edit haus.entity

--- src/haus/entity/haus.entity.ts
+++ src/haus/entity/haus.entity.ts
@@ -1,7 +1,42 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
-
+import {
+    Column,
+    Entity,
+    OneToMany,
+    OneToOne,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Ausstattung } from './ausstattung.entity';
+import { Bewohner } from './bewohner.entity';
 @Entity()
 export class Haus {
     @PrimaryGeneratedColumn()
     id: number | undefined;
+
+    @Column('varchar')
+    art: string | undefined;
+
+    @Column('int')
+    stockwerk: number | undefined;
+
+    @Column('int')
+    zimmer: number | undefined;
+
+    @Column('decimal', { precision: 10, scale: 2 })
+    preis: number | undefined;
+
+    @Column('decimal', { precision: 10, scale: 2 })
+    groesse: number | undefined;
+
+    @Column('varchar')
+    standort: string | undefined;
+
+    @OneToOne(() => Ausstattung, (ausstattung) => ausstattung.haus, {
+        cascade: ['insert', 'remove'],
+    })
+    readonly ausstattung: Ausstattung | undefined;
+
+    @OneToMany(() => Bewohner, (bewohner) => bewohner.haus, {
+        cascade: ['insert', 'remove'],
+    })
+    readonly bewohner: Bewohner[] | undefined;
 }
